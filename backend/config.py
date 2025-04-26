d7a8fac73f5e84b9bda21fa3b195db5fe5c153ea
Soaicl-as <social.marketing638@gmail.com>
2025-04-25 18:43:27 -0700

    Create config.py

---
+++ backend/config.py
@@ -0,0 +1,11 @@
+import os
+
+class Config:
+    INSTAGRAM_USERNAME = ""
+    INSTAGRAM_PASSWORD = ""
+    TARGET_USERNAME = ""
+    TARGET_LIST = ""  # followers or following
+    MESSAGE = ""
+    NUM_RECIPIENTS = 0
+    DELAY_MIN = 0
+    DELAY_MAX = 0
